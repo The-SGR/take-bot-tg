8043a5325cb4d8bdfe91f1346eca419ffceb688f
SGR <113258080+The-SGR@users.noreply.github.com>
2025-05-25 17:26:55 +0300

    чёт обновил
    >w<

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start_msg(message):
-    bot.send_message(message.chat.id,"День добрый!\nНапиши сюда свой тейк и мы его, возможно, опубликуем!")
+    bot.send_message(message.chat.id,"<b>День добрый!</b>\nНапиши сюда свой тейк и мы его, возможно, опубликуем!\n\nЭтот бот был сделан с помощью сгруши и пайтона (/github)", parse_mode='html')
 
 @bot.message_handler(commands=['github'])
 def github_msg(message):
@@ -16,6 +16,7 @@ def forward_msg(message):
     user_id = message.from_user.id
 
     if user_id not in admins:
+        bot.reply_to(message, "<b>Сообщение успешно отправлено!</b>", parse_mode='html')
         for admin in admins:
             try:
                 bot.forward_message(
